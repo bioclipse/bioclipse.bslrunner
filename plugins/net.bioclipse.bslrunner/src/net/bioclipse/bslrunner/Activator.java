58995440abc982092749409ee7987a95178024bb
jonalv <jonathan.alvarsson@gmail.com>
2010-12-10 16:54:50 +0100

    code for starting the Spring extender bundle

--- plugins/net.bioclipse.bslrunner/src/net/bioclipse/bslrunner/Activator.java
+++ plugins/net.bioclipse.bslrunner/src/net/bioclipse/bslrunner/Activator.java
@@ -1,11 +1,20 @@
 package net.bioclipse.bslrunner;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
 
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
+
+    private static final String EXTENDER_BUNDLE_NAME =
+        "org.springframework.bundle.osgi.extender";
 
 	static BundleContext getContext() {
 		return context;
@@ -18,6 +27,7 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 	    System.out.println("net.bioclipse.bslrunner started");
 		Activator.context = bundleContext;
+		startBundleExtender();
 	}
 
 	/*
@@ -28,4 +38,24 @@ public class Activator implements BundleActivator {
 		Activator.context = null;
 	}
 
+    private void startBundleExtender() {
+        List<Bundle> allExtenders =
+            Arrays.asList(Platform.getBundles(EXTENDER_BUNDLE_NAME, null));
+        if ( allExtenders.size() == 0 ) {
+            throw new RuntimeException("found no extender boundle");
+        }
+
+        for (Bundle b : allExtenders)
+            startTransiently(b);
+    }
+
+    private void startTransiently(Bundle b) {
+        try {
+            b.start(Bundle.START_TRANSIENT);
+            System.out.println("Extender boundle started");
+        }
+        catch (BundleException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
